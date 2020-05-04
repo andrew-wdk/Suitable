a195e9efd29b8a36cf14481b394baf5949647012
andrew <andrew.wdk@gmail.com>
2020-05-04 21:02:10 +0200

    fixed datatable in admin.events and show button

--- resources/views/admin/admin-events.blade.php
+++ resources/views/admin/admin-events.blade.php
@@ -8,14 +8,15 @@
                 <div class="card-header">Events</div>
 
                     <div class="card-body">
-            
-                    <table class="table">
+
+                    <table id="table" class="table">
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Title</th>
                             <th>Host</th>
                             <th>Created at</th>
+                            <th>Action</th>
                         </thead>
                         <tbody>
 
@@ -25,17 +26,27 @@
                             <td>{{$event['id']}}</td>
                             <td>{{$event['title']}}</td>
                             <td>{{$hosts[$i]->name}}</td>
-                            <td>{{$event['created_at']->diffForhumans()}}</td>                            
-                            <td>{{ Form::open(['method' => 'DELETE', 'action' => ['EventsController@destroy', $event->id]]) }}
-                                {{ Form::submit('delete')}}
-                                {{ Form::close()}}
+                            <td>{{$event['created_at']->diffForhumans()}}</td>
+                            <td>
+                                <div class="row">
+                                    <div>
+                                        {{ Form::open(['method' => 'DELETE', 'action' => ['EventsController@destroy', $event->id]]) }}
+                                        {{ Form::submit('delete')}}
+                                        {{ Form::close()}}
+                                    </div>
+                                    <div>
+                                        {{ Form::open(['method' => 'GET', 'action' => ['EventsController@show', $event->id]]) }}
+                                        {{ Form::submit('show')}}
+                                        {{ Form::close()}}
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                             @endforeach
                         @endif
-                        
+
                     </tbody>
-                            
+
                     </table>
                     <div class ="row justify-content-center">
                         {{$events->links()}}
@@ -49,4 +60,12 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    $(document).ready( function () {
+    $('#table').DataTable();
+    } );
+</script>
 @endsection
